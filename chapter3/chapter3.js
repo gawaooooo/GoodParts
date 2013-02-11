a91760ccb7c107ab6ca0a8870dcad192fd050688
gawao <gawao57@gmail.com>
2013-02-11 22:16:40 +0900

    3.7 update P.28

--- chapter3/chapter3.js
+++ chapter3/chapter3.js
@@ -91,3 +91,14 @@ for(name in another_stooge) {
 		document.writeln(name + ': ' + another_stooge[name]);
 	}
 }
+
+var i;
+var properties = [
+	'first-name',
+	'middle-name',
+	'last-name',
+	'profession'
+];
+for(i = 0; i < properties.length; i += 1) {
+	document.writeln(properties[i] + ': ' + another_stooge[properties[i]]);
+}
